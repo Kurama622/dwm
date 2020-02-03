72e33ceba5a8c0bb647d3fdd1a8638977da625ca
demonlord1997 <2273893094@qq.com>
2020-02-03 17:06:15 +0800

    reconfigure color

--- config.h
+++ config.h
@@ -1,41 +1,37 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 2;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 2;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 2;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 2;       /* vert outer gap between windows and screen edge */
-static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
-static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
-static const Bool viewontag         = True;     /* Switch view on tag switch */
-static const char *fonts[]          = { "Fira Code:size=12" };
-static const char dmenufont[]       = "Fira Code:size=12";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-//static const char col_gray4[]       = "#eeeeee";
-static const char col_gray4[]       = "#ffffff";
-//static const char col_cyan[]        = "#005577";
-static const char col_cyan[]        = "#37474F";
-static const char col_border[]      = "#42A5F5";
-static const unsigned int baralpha  = 0xd0;
+static const unsigned int borderpx    = 1;        /* border pixel of windows */
+static const unsigned int snap        = 32;       /* snap pixel */
+static const unsigned int gappih      = 2;       /* horiz inner gap between windows */
+static const unsigned int gappiv      = 2;       /* vert inner gap between windows */
+static const unsigned int gappoh      = 2;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov      = 2;       /* vert outer gap between windows and screen edge */
+static const int smartgaps            = 0;        /* 1 means no outer gap when there is only one window */
+static const int showbar              = 1;        /* 0 means no bar */
+static const int topbar               = 1;        /* 0 means bottom bar */
+static const Bool viewontag           = True;     /* Switch view on tag switch */
+static const char *fonts[]            = { "Fira Code:size=12" };
+static const char dmenufont[]         = "Fira Code:size=12";
+static const char col_gray1[]         = "#222222";
+static const char col_gray2[]         = "#444444";
+static const char col_gray3[]         = "#bbbbbb";
+static const char col_gray4[]         = "#ffffff";
+static const char col_cyan[]          = "#090909";
+static const char col_border[]        = "#42A5F5";
+static const unsigned int baralpha    = 0xd0;
 static const unsigned int borderalpha = OPAQUE;
-//static const unsigned int borderalpha = 0xffU;
-static const char *colors[][3]      = {
+
+static const char *colors[][3]        = {
     /*               fg         bg         border   */
     [SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
     [SchemeSel]  = { col_gray4, col_cyan,  col_border},
-    //[SchemeHid]  = { col_cyan, col_gray1,  col_border},
 };
-static const unsigned int alphas[][3]      = {
-    /*               fg      bg        border     */
-    //[SchemeNorm] = { OPAQUE, baralpha, borderalpha },
-    //[SchemeSel]  = { OPAQUE, baralpha, borderalpha },
-    [SchemeNorm] = { 0xffU, 0xc0, 0xffU },
-    [SchemeSel]  = { 0xffU, 0xc0, 0xffU },
+
+static const unsigned int alphas[][3] = {
+    /*               fg         bg         border   */
+    [SchemeNorm] = { 0xffU,     0xd0,      0xffU     },
+    [SchemeSel]  = { 0xffU,     0xd0,      0xffU     },
 };
 
 /* tagging */
@@ -84,7 +80,6 @@ static const char *firefoxcmd[]    = { "firefox", NULL };
 static const char *chromiumcmd[]   = { "chromium", NULL };
 static const char *nautiluscmd[]   = { "nautilus", NULL };
 static const char *shuttercmd[]    = { "/home/vegeta/scripts/shutter_start.sh", NULL };
-//static const char *octavecmd[]     = { "/home/vegeta/scripts/octave_start.sh", NULL };
 static const char *trayercmd[]     = { "/home/vegeta/scripts/tr-start.sh", NULL };
 static const char *wallpapercmd[]  = { "/home/vegeta/Pictures/wallpapers/wallpaper.sh", NULL };
 static const char *vpncmd[]        = { "/home/vegeta/scripts/vpn.sh", NULL };
@@ -105,7 +100,6 @@ static Key keys[] = {
     { MODKEY|ShiftMask,           XK_a,                    spawn,          {.v = shuttercmd } },
     { MODKEY|ShiftMask,           XK_v,                    spawn,          {.v = vpncmd } },
     { MODKEY|ShiftMask,           XK_u,                    spawn,          {.v = nautiluscmd } },
-    //{ MODKEY|ShiftMask,           XK_m,                    spawn,          {.v = octavecmd } },
     { 0,                          XF86XK_AudioLowerVolume, spawn,          {.v = downvol} },
     { 0,                          XF86XK_AudioRaiseVolume, spawn,          {.v = upvol} },
     { 0,                          XF86XK_AudioMute,        spawn,          {.v = mutevol} },
