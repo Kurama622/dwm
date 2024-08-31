6d528098fc974417697f9b650e461a04950df935
Kurama <kurama174@163.com>
2024-08-31 19:58:26 +0800

    update scripts

---
+++ scripts/autostart.sh
@@ -0,0 +1,10 @@
+# feh --bg-fill ~/Pictures/wallpapers/grave.jpg
+# compton -b
+xfce4-power-manager &
+# bash ~/scripts/status-refresh.sh &
+xset s 0
+xset dpms 0 0 0
+fcitx5 &
+./tap_to_click.sh
+./setxkbmap-qwery.sh
+#xsetroot -name "vegeta"
