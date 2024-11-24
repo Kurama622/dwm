06a5be826e1a1a6450e8e53f8d2ecdce6c2432cf
Kurama <kurama174@163.com>
2024-11-24 21:19:13 +0800

    update text pos

--- dwm.c
+++ dwm.c
@@ -775,7 +775,7 @@ drawbar(Monitor *m)
     /* draw status first so it can be overdrawn by tags later */
     if (m == selmon) { /* status is only drawn on selected monitor */
         // drw_setscheme(drw, scheme[SchemeNorm]);
-        sw = TEXTW(stext) - lrpad + 2; /* 2px right padding */
+        sw = TEXTW(stext) - 2*lrpad; /* 2px right padding */
         // drw_text(drw, m->ww - sw, 0, sw, bh, 0, stext, 0);
         while (1) {
           if ((unsigned int)*ts > LENGTH(colors)) { ts++; continue ; }
